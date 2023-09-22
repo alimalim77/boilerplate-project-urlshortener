ed796cb7852fba60d2072241bba7ef7613f0427c
Replit user <>
2023-09-22 18:15:10 +0530

    URL_Shortener_FCC

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+const isUrl = require("is-url");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+let counter = 0;
+const shortenedUrls = {};
+const cur_url = "https://boilerplate-project-urlshortener.mohammadallie.repl.co"
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+  if (!isUrl(url)) {
+    res.send({ error: 'invalid url' })
+    return;
+  }
+  counter += 1;
+  shortenedUrls[counter] = url;
+  res.send({ original_url: req.body.url, short_url: cur_url + "/api/shorturl/" + counter });
+})
+
+app.get('/api/shorturl/:id', function(req, res) {
+  id = req.params.id;
+  actual = shortenedUrls[id];
+  res.redirect(actual)
 });
 
 // Your first API endpoint
